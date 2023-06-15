8be4fd3bbeb18290715d87ef95a89ddf781fdec3
Anshu Anand <109796743+Anshuanand58@users.noreply.github.com>
2023-06-15 11:36:19 +0530

    Update sb-ci-ecom-dev-srch-02.tf

--- sb-ci-ecom-dev-srch-02.tf
+++ sb-ci-ecom-dev-srch-02.tf
@@ -1,10 +1,10 @@
-module "sb-ci-ecom-dev-srch-02" {
+module "test-service_bus" {
   source                = "git::https://xkmjl6xcuanap5betn3m43lagh5y6llpn6lq5bqfpaxep7cynvca@dev.azure.com/IRL-INFRA-DEVOPS/IRL-MODULES/_git/IRL-MODULES//azurerm_servicebus_namespace"
   create_resource_group = true
-  resource_group_name   = "rsg-ci-ecom-dev-srch-01"
+  resource_group_name   = "test-rg"
   location              = "centralindia"
   servicebus = {
-    name               = "sb-ci-ecom-dev-srch-02"
+    name               = "test-service_bus"
     sku                = "Basic"
     local_auth_enabled = true
     zone_redundant     = false
@@ -13,28 +13,11 @@ module "sb-ci-ecom-dev-srch-02" {
 
   queue = {
     "queue1" = {
-      name                  = "catalog-dev"
+      name                  = "queue1
       enable_partitioning   = false
       max_size_in_megabytes = "1024"
       status                = "Active"
       max_delivery_count    = 3
       lock_duration         = "30"
     },
-    "queue2" = {
-      name                  = "catalog-pt"
-      enable_partitioning   = false
-      max_size_in_megabytes = "1024"
-      status                = "Active"
-      max_delivery_count    = 3
-      lock_duration         = "30"
-    },
-    "queue3" = {
-      name                  = "catalog-uat"
-      enable_partitioning   = false
-      max_size_in_megabytes = "1024"
-      status                = "Active"
-      max_delivery_count    = 3
-      lock_duration         = "30"
-    },
-  }
 }
